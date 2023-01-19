4eaf0185d1ed4a6cd3e68fb975818cabdbc279c9
Arpan Sarkar <arpansarkar45@gmail.com>
2023-01-19 22:08:13 +0530

    setup

---
+++ admin/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Registration from './pages/Registration'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
